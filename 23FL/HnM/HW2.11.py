4a543a6f4f4ff7878b355bf20e3ffe88a792fddb
Jakob Werle <jakob.werle@temple.edu>
2023-09-13 00:53:34 -0400

    corrected solve method for q and cost

--- 23FL/HnM/HW2.11.py
+++ 23FL/HnM/HW2.11.py
@@ -3,6 +3,7 @@ import numpy as np
 L = 0.4
 W = 0.4
 A = L*W
+x = 0.012
 h_i = 1
 h_o = 1
 delta_T = 90
@@ -13,15 +14,14 @@ no_hrs = 8
 
 K_ag = 0.014
 K_pc = 0.21
-K_values = np.array([K_ag, K_pc])
+K_Ls = 1.4
+K_values = np.array([K_ag, K_pc, K_Ls])
 
 results = []
 
 for K in K_values:
-    # R_tot = (1 / A) * (1 / h_i + L / K + 1 / h_o)
-    R_tot = (1 / A) * (L / K)
-    q = delta_T / R_tot
-    C_tot = no_wndws * q * pwr_cost * no_hrs
+    q = -K * A * delta_T / x
+    C_tot = no_wndws * -q * pwr_cost * no_hrs
 
     results.append(C_tot)
 
